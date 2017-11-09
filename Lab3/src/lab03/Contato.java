595544f7b9b74efb63249be2c120d48bc5daa3a8
Joao Marcelo <marcelinhofernandes2013@gmail.com>
2017-11-08 22:34:39 -0300

    Primeiro Commit

---
+++ Lab3/src/lab03/Contato.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab03;
+
+/**
+ *
+ * @author joao
+ */
+public class Contato {
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    
+    public Contato(String nome, String sobrenome, String telefone){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+    }
+}
