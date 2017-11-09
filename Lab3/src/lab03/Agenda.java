629ef295405446052394cbae00e450a8e81f18de
Joao Marcelo <marcelinhofernandes2013@gmail.com>
2017-11-09 20:30:59 -0300

    Excecoes

--- Lab3/src/lab03/Agenda.java
+++ Lab3/src/lab03/Agenda.java
@@ -11,14 +11,23 @@ public class Agenda {
         this.contatos = new Contato[100];
     }
     
-    public boolean cadastrarContatos(int posicao, String nome, String telefone){
+    private void testaNome(String nome, String sobrenome){
+        if(nome==null || sobrenome==null) throw new NullPointerException();
+        if(nome=="" || sobrenome=="") throw new IllegalArgumentException();
+        int cont = 0;
+        for(int i=0; i<nome.length();i++){
+            if(nome.charAt(i) == ' ') cont++;
+        }
+        if(cont==nome.length()) throw new IllegalArgumentException();
+    }
+    public boolean cadastrarContatos(int posicao, String nome, String sobrenome, String telefone){
         if(posicao < 1 || posicao > 100) return false;
-        this.contatos[--posicao] = new Contato(nome, telefone);
+        this.contatos[--posicao] = new Contato(nome, sobrenome, telefone);
         return true;
     }
     
     public String pesquisarContato(int posicao){
-        if(contatos[posicao-1]==null) return "POSIÇÃO INVÁLIDA";
+        if(contatos[posicao-1]==null) throw new NullPointerException();
         return contatos[posicao-1].toString();
     }
     
--- Lab3/src/lab03/Contato.java
+++ Lab3/src/lab03/Contato.java
@@ -11,11 +11,12 @@ package lab03;
  */
 public class Contato {
     private String nome;
-
+    private String sobrenome;
     private String telefone;
     
-    public Contato(String nome, String telefone){
+    public Contato(String nome, String sobrenome, String telefone){
         this.nome = nome;
+        this.sobrenome = sobrenome;
         this.telefone = telefone;
     }
     
