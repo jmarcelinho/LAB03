07c8fc017d990641497a0b6aa9b83eea2c6f1836
kauan <kauan>
2017-12-03 17:16:44 -0200

    Documentao de grupo

--- Lab4/src/lab04/ControleGrupos.java
+++ Lab4/src/lab04/ControleGrupos.java
@@ -13,11 +13,24 @@ import java.util.HashMap;
 public class ControleGrupos {
 	private HashMap <String, Grupo> grupos;
 	
-	
+	/**
+	 * Constroi um sistema de controle de grupos.
+	 * No sistem nao podem ser cadastrados grupos
+	 * com nomes iguais.
+	 */
 	public ControleGrupos() {
 		this.grupos = new HashMap<>();
 	}
 	
+	/**
+	 * Cadastra um novo grupo no sistema.
+	 * Um grupo eh cadastrado a partir de um nome.
+	 * O grupo eh cadastrado com sucesso se no sistema 
+	 * nao existir um grupo com o mesmo nome.
+	 * @param nome nome do grupo a ser adicionado.
+	 * @return True se o cadastro for realizado com sucesso
+	 * e False caso contrario.
+	 */
 	public boolean cadastrarGrupo(String nome) {
 		if(contemGrupo(nome)) {
 			return false;
@@ -26,6 +39,17 @@ public class ControleGrupos {
 		return true;
 	}
 	
+	/**
+	 * Aloca um aluno a um determinado grupo.
+	 * Para essa acao eh necessaria informar o nome de um grupo 
+	 * valido e informacoes de um aluno a ser adicionado.
+	 * Retorna True se o grupo for valido e o cadastro ocorrer com sucesso
+	 * e False caso contrario.
+	 * @param nomeGrupo nome do grupo para alocao.
+	 * @param aluno aluno que deve ser alocado no grupo.
+	 * @return True se o cadastro for realizado com sucesso
+	 * e False caso contrario.
+	 */
 	public boolean alocarAluno(String nomeGrupo, Aluno aluno){
 		if(!contemGrupo(nomeGrupo)){
 			throw new IllegalArgumentException("Grupo nao cadastrado");
@@ -33,6 +57,12 @@ public class ControleGrupos {
 		return grupos.get(nomeGrupo).adicionarAluno(aluno);
 	}
 	
+	/**
+	 * Retorna uma String com informacoes de um determinado grupo.
+	 * O grupo eh pesquisado a partir do seu nome.
+	 * @param nomeGrupo nome do grupo que se deseja obter informacoes.
+	 * @return String com as informacoes do grupo pesquisado.
+	 */
 	public String imprimirGrupo(String nomeGrupo){
 		if(!contemGrupo(nomeGrupo)){
 			throw new IllegalArgumentException("Grupo nao cadastrado");
@@ -40,7 +70,7 @@ public class ControleGrupos {
 		return grupos.get(nomeGrupo).toString();
 	}
 	
-	public boolean contemGrupo(String nome){
+	private boolean contemGrupo(String nome){
 		return grupos.containsKey(nome);
 	}
 	
